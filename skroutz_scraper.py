a919caaa18bdbc844c00c6761210da464d2e1651
abki12c <komnas2011@gmail.com>
2024-08-02 05:25:33 +0300

    updated price monitoring

--- skroutz_scraper.py
+++ skroutz_scraper.py
@@ -60,13 +60,12 @@ class skroutz_scraper(Base_Scraper):
             for product in products_list:
                 product_name = product["name"]
                 product_link = "https://www.skroutz.gr" + product["sku_url"]
-                if(product["obsolete"]):
+                if (product["obsolete"]):
                     # Product is unavailable
                     continue
                 else:
                     product_price = product["price"].replace('€', '').replace(".", "").replace(',', '.')
                     product_price = float(product_price)
-
 
                 product_info = {
                     "name": product_name,
@@ -76,8 +75,6 @@ class skroutz_scraper(Base_Scraper):
                 self.all_products.append(product_info)
 
             current_page_number += 1
-
-
 
     def select_products(self):
         product = input("Enter the product you're looking for on Skroutz: ")
@@ -97,14 +94,12 @@ class skroutz_scraper(Base_Scraper):
         response = session.get('https://www.skroutz.gr/search.json', params=self.params, headers=self.headers)
         response_data = response.json()
 
-
-        if("redirectUrl" in response_data):
+        if ("redirectUrl" in response_data):
             url = response_data["redirectUrl"].replace("html", "json")
             response = session.get(url, params=self.params, headers=self.headers)
             response_data = response.json()
 
-
-        if(self.categories_are_available(response_data)) :
+        if (self.categories_are_available(response_data)):
             categories = [{"name": category["name"], "link": "https://www.skroutz.gr/" + category["url"]} for category in response_data["page"]["category_cards"]]
 
             print()  # empty tile
@@ -124,13 +119,13 @@ class skroutz_scraper(Base_Scraper):
             category_number = int(category_number)
 
             # go to the right category
-            url = categories[category_number-1]["link"].replace("html","json")
-            response = session.get(url, headers=self.headers,params=self.params)
+            url = categories[category_number - 1]["link"].replace("html", "json")
+            response = session.get(url, headers=self.headers, params=self.params)
 
-            if(response.status_code==301):
+            if (response.status_code == 301):
                 new_url = response.headers['Location']
                 print(new_url)
-                response = requests.get(new_url,headers=self.headers)
+                response = requests.get(new_url, headers=self.headers)
             response_data = response.json()
 
         pages = response_data['page']['total_pages']
@@ -145,7 +140,6 @@ class skroutz_scraper(Base_Scraper):
 
         # Select product number(s) and save them in a list
         self.selected_products.extend(self.select_items(products_number))
-
 
     def save_products_to_csv(self):
         product = input("Enter the product you're looking for on Skroutz: ")
@@ -203,9 +197,9 @@ class skroutz_scraper(Base_Scraper):
 
         self.process_items(pages, response)
 
-        with open("data/info.csv","w",encoding="utf-8-sig", newline='') as csvfile:
-            fieldnames = ["name","link","price"]
-            writer = csv.DictWriter(csvfile,fieldnames=fieldnames)
+        with open("data/info.csv", "w", encoding="utf-8-sig", newline='') as csvfile:
+            fieldnames = ["name", "link", "price"]
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writeheader()
             for product in self.all_products:
                 product['name'] = product['name'].encode('utf-8', errors='replace').decode('utf-8', errors='replace')
@@ -213,15 +207,15 @@ class skroutz_scraper(Base_Scraper):
 
         print("file has been saved")
 
-    def monitor_product(self, checking_frequency = 60):
+    def monitor_product(self, checking_frequency=60):
         # if the csv file for monitoring products doesn't exist, create it
-        if(not os.path.exists("monitored_products.csv")):
-            with open("monitored_products.csv", "w", newline="") as csvfile:
+        if (not os.path.exists("data/monitored_products.csv")):
+            with open("data/monitored_products.csv", "w", newline="") as csvfile:
                 header = ["Name", "Price_Alert", "Current_Price", "Link"]
                 csvwriter = csv.writer(csvfile)
                 csvwriter.writerow(header)
 
-        with open("monitored_products.csv","r+",newline="") as csvfile:
+        with open("data/monitored_products.csv", "r+", newline="") as csvfile:
             csvreader = csv.reader(csvfile)
             csvwriter = csv.writer(csvfile)
 
@@ -235,31 +229,31 @@ class skroutz_scraper(Base_Scraper):
                 # If StopIteration is raised, there are no more rows
                 products_available = False
 
-            if (products_available is False):
+            if (not products_available):
                 link = input("There's no available products in the csvfile. Enter the link here: ")
                 idx = link.rindex("/")
-                new_link = link[:idx+1] + "filter_products.json?"
+                new_link = link[:idx + 1] + "filter_products.json?"
                 name = input("Enter the product name: ")
                 response = self.session.get(new_link, headers=self.headers)
                 response_data = response.json()
-                current_price = response_data["price_min"].replace(",",".").replace("€","")
+                current_price = response_data["price_min"].replace(",", ".").replace("€", "")
                 current_price = float(current_price)
-                price_alert = int(input(f"Enter the price point at which you want to receive a notification if the price is the same or lower. The current price is {current_price}: "))
-                row_data = [name,price_alert,current_price,link]
+                price_alert = int(input(
+                    f"Enter the price point at which you want to receive a notification if the price is the same or lower. The current price is {current_price}: "))
+                row_data = [name, price_alert, current_price, link]
                 csvwriter.writerow(row_data)
-
 
             # show the available products to monitor
             products_row_numbers = []
             row_number = 1
             csvfile.seek(0)
-            print() # empty line
+            print()  # empty line
             for row in csvreader:
-                if len(row)>0 and row!=['Name', 'Price_Alert', 'Current_Price', 'Link']:
-                    print("Row: " + str(row_number) + "-> Name: " + row[0] + ", Price_Alert: ", row[1] + ", Current_Price: " + row[2] + ", Link: " + row[3])
+                if len(row) > 0 and row != ['Name', 'Price_Alert', 'Current_Price', 'Link']:
+                    print("Row: " + str(row_number) + "-> Name: " + row[0] + ", Price_Alert: ",
+                          row[1] + ", Current_Price: " + row[2] + ", Link: " + row[3])
                     products_row_numbers.append(row_number)
-                row_number+=1
-
+                row_number += 1
 
             print()  # empty line
             print("-----------------------------")
@@ -272,7 +266,7 @@ class skroutz_scraper(Base_Scraper):
                     if (int(product_numbers_string) not in products_row_numbers):
                         product_numbers_string = input("Wrong number, enter again: ")
                         commas = product_numbers_string.count(",")
-                    elif(product_numbers_string.lower() == "all"):
+                    elif (product_numbers_string.lower() == "all"):
                         product_numbers_list.append(products_row_numbers)
                         product_numbers_are_incorrect = False
                     else:
@@ -280,8 +274,8 @@ class skroutz_scraper(Base_Scraper):
                         product_numbers_are_incorrect = False
                 else:
                     product_numbers_string = product_numbers_string.split(",")
-                    if (any(int(product_number) not in products_row_numbers for product_number in
-                            product_numbers_string)):
+                    if (
+                    any(int(product_number) not in products_row_numbers for product_number in product_numbers_string)):
                         product_numbers_string = input("One or more numbers are incorrect, enter again: ")
                         commas = product_numbers_string.count(",")
                     else:
@@ -290,37 +284,38 @@ class skroutz_scraper(Base_Scraper):
 
             print()
             print("-----------------------------")
-            # save the selected products
-            selected_products = [self.all_products[product_number - 1] for product_number in product_numbers_list]
-
-            while(product_number not in products_row_numbers):
-                product_number = int(input("The provided row number is incorrect, enter again: "))
-
-            csvfile.seek(0)
-
-            for _ in range(product_number-1):
-                next(csvreader)
-
-            name, price_alert, current_price, link = next(csvreader)
-            product = {
-                "name": name,
-                "price_alert": float(price_alert),
-                "current_price": current_price,
-                "link": link,
-                "product_number": product_number
-            }
 
 
+
+            product_list = []
+            for i in range(len(product_numbers_list)):
+
+                csvfile.seek(0)
+
+                for _ in range(product_numbers_list[i] - 1):
+                    next(csvreader)
+
+                name, price_alert, current_price, link = next(csvreader)
+                product = {
+                    "name": name,
+                    "price_alert": float(price_alert),
+                    "current_price": current_price,
+                    "link": link,
+                    "product_number": product_numbers_list[i]
+                }
+
+                product_list.append(product)
 
             print("Price Monitoring has began", end="\n\n")
-            while(True):
+            new_rows = []
+            while (True):
                 idx = product["link"].rindex("/")
                 new_link = link[:idx + 1] + "filter_products.json?"
                 response = self.session.get(new_link, headers=self.headers)
                 response_data = response.json()
                 # check
                 current_minimum_price = float(response_data["price_min"].replace('€', '').replace(',', '.'))
-                if(current_minimum_price<product["price_alert"]):
+                if (current_minimum_price < product["price_alert"]):
                     print(f"There's a new minimum price for {product['name']} which is {current_minimum_price}€")
                     print(f"Link: {product['link']}")
                     csvfile.seek(0)
@@ -329,19 +324,20 @@ class skroutz_scraper(Base_Scraper):
                         if line_number == product["product_number"]:
                             # Modify the third column
                             row[2] = current_minimum_price
-                            new_row = row
+                            new_rows.append(row)
                             break
                     break
                 print("Checked the current price. Price hasn't changed")
                 time.sleep(checking_frequency)
 
         # open the file to read the existing data and change the new data with the new minimum price
-        with open("monitored_products.csv", "r", newline="") as csvfile:
+        with open("data/monitored_products.csv", "r", newline="") as csvfile:
             csvreader = csv.reader(csvfile)
             existing_data = list(csvreader)
-            existing_data[product["product_number"] - 1] = new_row
+            for i in range(len(product_numbers_list)):
+                existing_data[product_list[i]["product_number"] - 1] = new_rows[i]
 
         # open the file to write the new minimum price
-        with open("monitored_products.csv", "w", newline="") as csvfile:
+        with open("data/monitored_products.csv", "w", newline="") as csvfile:
             csvwriter = csv.writer(csvfile)
             csvwriter.writerows(existing_data)
